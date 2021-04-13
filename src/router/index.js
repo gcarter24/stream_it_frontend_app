ab19320608193046ee5af074ef1f99c6f2e573c7
George <gchriscarter@gmail.com>
2021-04-13 12:15:46 -0400

    create show action, test to make sure data shows up

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import MovieShow from "../views/MovieShow.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/movies/:id",
+    name: "MovieShow",
+    component: MovieShow,
   },
   {
     path: "/about",
---
+++ src/views/MovieShow.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="movies-show">
+    <h1>{{ message }}</h1>
+    <!-- <p>
+      <b>Id:</b>
+      {{ movie.id }}
+    </p> -->
+    <p>
+      <b>Title:</b>
+      {{ movie.title }}
+    </p>
+    <p><img v-bind:src="movie.image" /></p>
+    <p>
+      <b>Plot:</b>
+      {{ movie.plot }}
+    </p>
+    <p>
+      <b>Year:</b>
+      {{ movie.year }}
+    </p>
+    <p>
+      <b>Rating:</b>
+      {{ movie.rating }}
+    </p>
+    <p>
+      <b>Run-time:</b>
+      {{ movie.runtime_minutes }}
+    </p>
+    <p>
+      <b>Type:</b>
+      {{ movie.media_type }}
+    </p>
+    <p>
+      <b>Language:</b>
+      {{ movie.language }}
+    </p>
+    <p>
+      <b>Genres:</b>
+      {{ movie.genres }}
+    </p>
+    <p>
+      <b>Network:</b>
+      {{ movie.network }}
+    </p>
+  </div>
+</template>
+<style></style>
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the Show!",
+      movie: {},
+    };
+  },
+  created: function () {
+    console.log(this.$route.params.id);
+    axios.get(`/api/movies/${this.$route.params.id}`).then((response) => {
+      console.log(response.data);
+      this.movie = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
