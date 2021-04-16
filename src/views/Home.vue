90a38429bdfb67a24f6b7ef29abea037384f8c47
George <gchriscarter@gmail.com>
2021-04-16 15:59:10 -0400

    switch random.vue with home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,72 +1,43 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p>
-      Year:
-      <input type="text" v-model="year" placeholder="Year" />
-      Rating:
-      <input type="text" v-model="rating" placeholder="Rating" />
-      Length:
-      <input type="text" v-model="runtime_minutes" placeholder="Length" />
-      Type:
-      <input type="text" v-model="media_type" placeholder="Type" />
-      Language:
-      <input type="text" v-model="language" placeholder="Language" />
-      Network:
-      <input type="text" v-model="network" placeholder="Network" />
-      Genre:
-      <input type="text" v-model="genre" placeholder="Genre" />
-    </p>
-    <button v-on:click="anyRandomMovie()">Any Random Movie</button>
-    <p>{{ randomMovie.title }}</p>
-    <img v-bind:src="randomMovie.image" />
-    <p>{{ randomMovie.plot }}</p>
-    <p>{{ randomMovie.year }}</p>
-    <p>{{ randomMovie.rating }}</p>
-    <p>{{ randomMovie.runtime_minutes }}</p>
-    <p>{{ randomMovie.media_type }}</p>
-    <p>{{ randomMovie.language }}</p>
-    <p>
-      {{ randomMovie.genre }}
-    </p>
-    <p>{{ randomMovie.network }}</p>
-    <!-- <p>
-      <b>Title:</b>
-      {{ movie.title }}
-    </p>
-    <p><img v-bind:src="movie.image" /></p>
-    <p>
-      <b>Plot:</b>
-      {{ movie.plot }}
-    </p>
-    <p>
-      <b>Year:</b>
-      {{ movie.year }}
-    </p>
-    <p>
-      <b>Rating:</b>
-      {{ movie.rating }}
-    </p>
-    <p>
-      <b>Run-time:</b>
-      {{ movie.runtime_minutes }} minutes
-    </p>
-    <p>
-      <b>Type:</b>
-      {{ movie.media_type }}
-    </p>
-    <p>
-      <b>Language:</b>
-      {{ movie.language }}
-    </p>
-    <p>
-      <b>Genres:</b>
-      {{ movie.genres }}
-    </p>
-    <p>
-      <b>Network:</b>
-      {{ movie.network }}
-    </p> -->
+    <form v-on:submit.prevent="randomMovie()">
+      <ul>
+        <li class="text-danger" v-for="error in errors" v-bind:key="error">
+          {{ error }}
+        </li>
+      </ul>
+      <p>
+        Year:
+        <input type="text" v-model="year" placeholder="Year" />
+        Rating:
+        <input type="text" v-model="rating" placeholder="Rating" />
+        Length:
+        <input type="text" v-model="runtime_minutes" placeholder="Length" />
+        Type:
+        <input type="text" v-model="media_type" placeholder="Type" />
+        Language:
+        <input type="text" v-model="language" placeholder="Language" />
+        Network:
+        <input type="text" v-model="network" placeholder="Network" />
+        Genre:
+        <!-- <li v-for="genre in genres" :key="genre.name">
+          {{ genre.name }}
+        </li> -->
+        <input type="text" v-model="genre" placeholder="Genre" />
+      </p>
+      <button v-on:click="randomMovie()">Any Random Movie</button>
+      <p>{{ final_movie.title }}</p>
+      <img v-bind:src="final_movie.image" />
+      <p>{{ final_movie.plot }}</p>
+      <p>{{ final_movie.year }}</p>
+      <p>{{ final_movie.rating }}</p>
+      <p>{{ final_movie.runtime_minutes }}</p>
+      <p>{{ final_movie.media_type }}</p>
+      <p>{{ final_movie.language }}</p>
+      <p>{{ final_movie.genres }}</p>
+      <p>{{ final_movie.network }}</p>
+    </form>
   </div>
 </template>
 <style></style>
@@ -78,33 +49,42 @@ export default {
       message: "Stream-It",
       year: "",
       rating: "",
-      runtime_minutes: "",
       media_type: "",
       language: "",
-      network: "",
+      runtime_minutes: "",
       genre: "",
-      randomMovie: {},
-      movie: {},
+      network: "",
+      final_movie: {},
+      errors: [],
     };
   },
-  created: function () {},
+  created: function () {
+    this.randomMovie();
+  },
   methods: {
-    anyRandomMovie: function () {
-      var params = {
-        year: this.year,
-        rating: this.rating,
-        runtime_minutes: this.runtime_minutes,
-        media_type: this.media_type,
-        language: this.language,
-        network: this.network,
-        genres: this.genre,
-      };
-      axios.get("/api/movies/random", params).then((response) => {
-        console.log(response.data);
-        this.randomMovie = response.data;
-      });
+    randomMovie: function () {
+      console.log("homepage in randomMovie");
+
+      axios
+        .get("/api/movies/random", {
+          params: {
+            year: this.year,
+            rating: this.rating,
+            media_type: this.media_type,
+            language: this.language,
+            runtime_minutes: this.runtime_minutes,
+            genre: this.genre,
+            network: this.network,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.final_movie = response.data;
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
     },
-   
   },
 };
 </script>
--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,35 +1,72 @@
 <template>
   <div class="movies-random">
     <h1>{{ message }}</h1>
-    <form>
-      <p>
-        Year:
-        <input type="text" v-model="year" placeholder="Year" />
-        Rating:
-        <input type="text" v-model="rating" placeholder="Rating" />
-        Length:
-        <input type="text" v-model="runtime_minutes" placeholder="Length" />
-        Type:
-        <input type="text" v-model="media_type" placeholder="Type" />
-        Language:
-        <input type="text" v-model="language" placeholder="Language" />
-        Network:
-        <input type="text" v-model="network" placeholder="Network" />
-        Genre:
-        <input type="text" v-model="genre" placeholder="Genre" />
-      </p>
-      <button @click="randomMovie()">Any Random Movie</button>
-      <p>{{ final_movie.title }}</p>
-      <img v-bind:src="final_movie.image" />
-      <p>{{ final_movie.plot }}</p>
-      <p>{{ final_movie.year }}</p>
-      <p>{{ final_movie.rating }}</p>
-      <p>{{ final_movie.runtime_minutes }}</p>
-      <p>{{ final_movie.media_type }}</p>
-      <p>{{ final_movie.language }}</p>
-      <p>{{ final_movie.genres }}</p>
-      <p>{{ final_movie.network }}</p>
-    </form>
+    <p>
+      Year:
+      <input type="text" v-model="year" placeholder="Year" />
+      Rating:
+      <input type="text" v-model="rating" placeholder="Rating" />
+      Length:
+      <input type="text" v-model="runtime_minutes" placeholder="Length" />
+      Type:
+      <input type="text" v-model="media_type" placeholder="Type" />
+      Language:
+      <input type="text" v-model="language" placeholder="Language" />
+      Network:
+      <input type="text" v-model="network" placeholder="Network" />
+      Genre:
+      <input type="text" v-model="genre" placeholder="Genre" />
+    </p>
+    <button v-on:click="anyRandomMovie()">Any Random Movie</button>
+    <p>{{ randomMovie.title }}</p>
+    <img v-bind:src="randomMovie.image" />
+    <p>{{ randomMovie.plot }}</p>
+    <p>{{ randomMovie.year }}</p>
+    <p>{{ randomMovie.rating }}</p>
+    <p>{{ randomMovie.runtime_minutes }}</p>
+    <p>{{ randomMovie.media_type }}</p>
+    <p>{{ randomMovie.language }}</p>
+    <p>
+      {{ randomMovie.genre }}
+    </p>
+    <p>{{ randomMovie.network }}</p>
+    <!-- <p>
+      <b>Title:</b>
+      {{ movie.title }}
+    </p>
+    <p><img v-bind:src="movie.image" /></p>
+    <p>
+      <b>Plot:</b>
+      {{ movie.plot }}
+    </p>
+    <p>
+      <b>Year:</b>
+      {{ movie.year }}
+    </p>
+    <p>
+      <b>Rating:</b>
+      {{ movie.rating }}
+    </p>
+    <p>
+      <b>Run-time:</b>
+      {{ movie.runtime_minutes }} minutes
+    </p>
+    <p>
+      <b>Type:</b>
+      {{ movie.media_type }}
+    </p>
+    <p>
+      <b>Language:</b>
+      {{ movie.language }}
+    </p>
+    <p>
+      <b>Genres:</b>
+      {{ movie.genres }}
+    </p>
+    <p>
+      <b>Network:</b>
+      {{ movie.network }}
+    </p> -->
   </div>
 </template>
 <style></style>
@@ -38,40 +75,34 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Random!",
-      year: "2021",
-      rating: "R",
-      media_type: "movie",
-      language: "English",
-      runtime_minutes: "242",
-      genre: "Action",
-      network: "HBO Max",
-      final_movie: {},
+      message: "Welcome to the Random Page!",
+      year: "",
+      rating: "",
+      runtime_minutes: "",
+      media_type: "",
+      language: "",
+      network: "",
+      genre: "",
+      randomMovie: {},
+      movie: {},
     };
   },
-  created: function () {
-    this.randomMovie();
-  },
+  created: function () {},
   methods: {
-    randomMovie: function () {
-      console.log("in random");
-      // var params = {
-      //   year: this.year,
-      //   rating: this.rating,
-      //   media_type: this.media_type,
-      //   language: this.language,
-      //   runtime_minutes: this.runtime_minutes,
-      //   genre: this.genre,
-      //   network: this.network,
-      // };
-      axios
-        .get(
-          "/api/movies/random?year=2021&rating=R&media_type=movie&language=English&runtime_minutes=242&genre=Action&network=HBO%20Max"
-        )
-        .then((response) => {
-          console.log(response.data);
-          this.final_movie = response.data;
-        });
+    anyRandomMovie: function () {
+      var params = {
+        year: this.year,
+        rating: this.rating,
+        runtime_minutes: this.runtime_minutes,
+        media_type: this.media_type,
+        language: this.language,
+        network: this.network,
+        genres: this.genre,
+      };
+      axios.get("/api/movies/random", params).then((response) => {
+        console.log(response.data);
+        this.randomMovie = response.data;
+      });
     },
   },
 };
